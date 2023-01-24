9e790fc094d0c0c17f39f4fc80b11a5047d1766e
jesus-molano <jessumolano@gmail.com>
2023-01-24 14:49:45 +0000

    Removed nanostore dependencies

--- src/stores/languageStore.tsx
+++
@@ -1,9 +0,0 @@
-import { atom } from 'nanostores'
-import * as dataEN from '../data/lang/en.json'
-import * as dataESP from '../data/lang/esp.json'
-
-export const language = atom(dataESP)
-
-export function setLanguage (isESP: boolean) {
-  isESP ? language.set(dataESP) : language.set(dataEN)
-}
