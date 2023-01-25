364dca2e4f2dd8222a74494c28ef6eb7fd5a510e
jesus-molano <jessumolano@gmail.com>
2023-01-25 19:31:18 +0000

    Some style changes in the header.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,6 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
-
-// https://astro.build/config
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
-});
+  integrations: []
+})
