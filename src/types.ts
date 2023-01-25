7d1ece9c96b36d9067a835618e0c0f135cbd79e8
jesus-molano <jessumolano@gmail.com>
2023-01-25 22:20:13 +0000

    Created content layout and types.ts

---
+++ src/types.ts
@@ -0,0 +1,56 @@
+export interface ContentLayoutProps {
+  navbarLinks?: Link[],
+  home: {
+    welcome: string
+    myself: string
+    rol: string
+  },
+  about: {
+    title: string
+    cards: AboutCard[]
+  },
+  projects: {
+    title: string
+    cards: ProjectsCard[]
+  },
+  contact: {
+    title: string
+    text: string
+    form: ContactForm
+  },
+  footer?: {
+    text: string
+    links: Link[]
+  }
+}
+
+export type AboutCard {
+  text: string
+  url: string
+  alt: string
+}
+
+export type ProjectsCard {
+  title: string,
+  imgURL: string,
+  definition: string,
+  githubURL: string,
+  technologies: {
+    name: string,
+    url: string,
+    icon: string
+  }[]
+  webURL: string,
+}
+
+export type ContactForm {
+  name: string
+  email: string
+  message: string
+  submit: string
+}
+
+export type Link {
+  text?: string
+  href: string
+}
