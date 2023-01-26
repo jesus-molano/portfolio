65d3316e989be8b87ce3acb81f98c5ddab077f81
jesus-molano <jessumolano@gmail.com>
2023-01-26 17:08:43 +0000

    Removed dynamic imports for images

--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,56 +1,56 @@
 export interface ContentLayoutProps {
-  navbarLinks?: Link[],
+  navbarLinks?: Link[]
   home: {
     welcome: string
     myself: string
     rol: string
-  },
+  }
   about: {
     title: string
     cards: AboutCard[]
-  },
+  }
   projects: {
     title: string
     cards: ProjectsCard[]
-  },
+  }
   contact: {
     title: string
     text: string
     form: ContactForm
-  },
+  }
   footer?: {
     text: string
     links: Link[]
   }
 }
 
-export type AboutCard {
+export type AboutCard = {
   text: string
   url: string
   alt: string
 }
 
-export type ProjectsCard {
-  title: string,
-  imgURL: string,
-  definition: string,
-  githubURL: string,
+export type ProjectsCard = {
+  title: string
+  imgURL: string
+  definition: string
+  githubURL: string
   technologies: {
-    name: string,
-    url: string,
+    name: string
+    url: string
     icon: string
   }[]
-  webURL: string,
+  webURL: string
 }
 
-export type ContactForm {
+export type ContactForm = {
   name: string
   email: string
   message: string
   submit: string
 }
 
-export type Link {
+export type Link = {
   text?: string
   href: string
 }
