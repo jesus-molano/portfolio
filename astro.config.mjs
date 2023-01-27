a30f6b4cbb342f87c76d8cf44274b997adc48db4
jesus-molano <jessumolano@gmail.com>
2023-01-27 03:57:37 +0000

    Added images to public directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
 import { defineConfig } from 'astro/config'
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [image()],
+  root: '.',
+  srcDir: './src',
+  publicDir: './public',
+  outDir: './dist',
   site: 'https://portfolio-seven-sable-76.vercel.app/',
-  base: '/'
+  base: '/',
+  trailingSlash: 'always',
+  build: {
+    format: 'directory'
+  },
+  server: {
+    host: 'localhost',
+    port: 3000
+  }
 })
