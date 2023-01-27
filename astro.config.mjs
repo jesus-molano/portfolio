37fd4fa4fb8a321602f40cfeb68ae980594e1ad4
jesus-molano <jessumolano@gmail.com>
2023-01-27 02:18:04 +0000

    Added site and base in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: []
+  integrations: [],
+  site: 'https://portfolio-seven-sable-76.vercel.app/',
+  base: '/'
 })
